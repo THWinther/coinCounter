7eb62ff0652c14fe0143f7f9e5fd0c1fcf29a791
THWinther <tho89@hi.is>
2019-11-26 13:09:48 +0000

    So far bætti við meira dóti

--- cointCounter.py
+++ cointCounter.py
@@ -1,6 +1,13 @@
 import json #notað til að vinna úr listunspent
 import os #notað til að kalla smileycoin-cli listunspent
 
-unspent = os.popen('ls') # tekur inn unix shell command og skilar hvað er prentað í breytu
-jason = unspent.read() # jason er núna útak úr os kallinu
-print(jason) # til að prufa hvað kemur
+
+def jsonSorter(js):
+    return js['amount']
+
+unspent = os.popen('smileycoin-cli listunspent') # tekur inn unix shell command og skilar hvað er prentað í breytu
+unspent = unspent.read() # jason er núna útak úr os kallinu
+jason = json.loads(unspent)
+smallest = sorted(jason,key=jsonSorter,reverse=False)
+print(jason[0])
+print(smallest[0])
