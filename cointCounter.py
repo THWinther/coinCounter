a612aa857e0782d77d8f0f01cd44bb4437177922
THWinther <tho89@hi.is>
2019-11-26 14:17:49 +0000

    Bara laga edge cases

--- cointCounter.py
+++ cointCounter.py
@@ -1,21 +1,38 @@
+# Höfundur Tómas Harry Ottósson, tho89@hi.is
+
 import json #notað til að vinna úr listunspent
 import os #notað til að kalla smileycoin-cli listunspent
 
-
+#Fall sem er comperator til að sortera færslum eftir amount
 def jsonSorter(js):
     return js['amount']
 
+#Gildi sem er notað til að byðja notandum um magn af færslum sem eiga að vera notaðar
 n=""
 while not isinstance(n, int):
     try:
-        n = int(input("Please pick number of txids you would like to clear!\n"))
+        n = int(input("Please write amount of transaction you would like to use\n"))
     except:
-        print("please write a valid integer!\n")
+        print("please pick a valid integer!\n")
 
 unspent = os.popen('smileycoin-cli listunspent') # tekur inn unix shell command og skilar hvað er prentað í breytu
 unspent = unspent.read() # jason er núna útak úr os kallinu
-jason = json.loads(unspent)
-smallest = sorted(jason,key=jsonSorter,reverse=False)
+jason = json.loads(unspent) # færir hreinan streng yfiri í JSON
+smallest = sorted(jason,key=jsonSorter,reverse=False) #Flokkar minnst til lengst 
+
+# Gildi sem sér um að segja notanda hversu mikið af smileycoins hann á
+# Til að nota í færsluna
+amount = 0.0
+
+# Passar að n er ekki lengra en hversu marga færslur notandi á
+if n > len(smallest):
+    n = len(smallest)
+    print(f'Total txid´s are: {len(smallest)}')
 
 for i in range(0,n):
-    print(smallest[i])
+    amount =amount+smallest[i].get('amount')
+print(f'Total coins available: {amount}')
+
+
+
+#Todo búa til json ið sjálft
