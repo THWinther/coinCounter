473baadb2dd56fd1bd472f1e24a4956a6979110d
THWinther <tho89@hi.is>
2019-11-26 13:39:54 +0000

    Added Try catch for n

--- cointCounter.py
+++ cointCounter.py
@@ -5,9 +5,17 @@ import os #notað til að kalla smileycoin-cli listunspent
 def jsonSorter(js):
     return js['amount']
 
+n=""
+while not isinstance(n, int):
+    try:
+        n = int(input("Please pick number of txids you would like to clear!\n"))
+    except:
+        print("please write a valid integer!\n")
+
 unspent = os.popen('smileycoin-cli listunspent') # tekur inn unix shell command og skilar hvað er prentað í breytu
 unspent = unspent.read() # jason er núna útak úr os kallinu
 jason = json.loads(unspent)
 smallest = sorted(jason,key=jsonSorter,reverse=False)
-print(jason[0])
-print(smallest[0])
+
+for i in range(0,n):
+    print(smallest[i])
